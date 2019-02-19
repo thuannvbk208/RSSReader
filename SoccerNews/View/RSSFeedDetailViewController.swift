c20b59284b7586366f2db7c73de7e2a685bac775
thuannvbk208 <thuannv208@gmail.com>
2019-02-19 21:32:13 +0700

    delete WKNavigationDelegate

--- SoccerNews/View/RSSFeedDetailViewController.swift
+++ SoccerNews/View/RSSFeedDetailViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class RSSFeedDetailViewController: UIViewController, WKNavigationDelegate {
+class RSSFeedDetailViewController: UIViewController {
     var selectedUrl: String?
     @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
@@ -17,8 +17,6 @@ class RSSFeedDetailViewController: UIViewController, WKNavigationDelegate {
         // Do any additional setup after loading the view.
         if let str = selectedUrl, let url = URL(string: str) {
             webView.load(URLRequest(url: url))
-            webView.navigationDelegate = self
-            webView.allowsBackForwardNavigationGestures = true
         } else {
             let alertController = UIAlertController(title: "Message Error", message: "Link invalid", preferredStyle: .alert)
             let doneAction = UIAlertAction(title: "Done", style: .default, handler: { (action) in
@@ -29,24 +27,4 @@ class RSSFeedDetailViewController: UIViewController, WKNavigationDelegate {
         }
     }
 }
-extension RSSFeedDetailViewController {
-    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-        print("the web view begins to receive web content.")
-    }
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        print("web content begins to load in a web view")
-    }
-    func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
-        print("web view receives a server redirect")
-    }
-    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        print("Called when the web view needs to respond to an authentication challenge.")
-        completionHandler(URLSession.AuthChallengeDisposition.performDefaultHandling, nil)
-    }
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("Called when the navigation is complete.")
-    }
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        decisionHandler(WKNavigationActionPolicy.allow)
-    }
-}
+
